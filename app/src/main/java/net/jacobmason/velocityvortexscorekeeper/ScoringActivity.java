b1f2f89b39d78e1e51763e061ab47d097b755812
Jacob Mason <jacob@jacobmason.net>
2016-12-12 20:44:45 -0600

    Dynamic button adding, haptic feedback, and score tallies

--- app/src/main/java/net/jacobmason/velocityvortexscorekeeper/ScoringActivity.java
+++ app/src/main/java/net/jacobmason/velocityvortexscorekeeper/ScoringActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.HapticFeedbackConstants;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -19,12 +21,16 @@ import java.util.Locale;
  * status bar and navigation/system bar) with user interaction.
  */
 public class ScoringActivity extends AppCompatActivity {
+    private TableLayout mContentView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Log.d("ScoringActivity", "Created Activity");
+
         setContentView(R.layout.activity_scoring_screen);
-        TableLayout mContentView = (TableLayout) findViewById(R.id.fullscreen_content);
+        mContentView = (TableLayout) findViewById(R.id.fullscreen_content);
         mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -38,21 +44,53 @@ public class ScoringActivity extends AppCompatActivity {
 
         switch (getIntent().getStringExtra("scoring_style")) {
             case "Red":
-            case "Red Corner":
-            case "Red Center":
+                mContentView.addView(new ScoringButton(this, "Center Goal"));
+                mContentView.addView(new ScoringButton(this, "Corner Goal"));
                 mContentView.setBackgroundColor(0xFFFF0000);
                 break;
+            case "Red Corner":
+                mContentView.addView(new ScoringButton(this, "Corner Goal"));
+                mContentView.setBackgroundColor(0xFFFF0000);
+                break;
+            case "Red Center":
+                mContentView.addView(new ScoringButton(this, "Center Goal"));
+                mContentView.setBackgroundColor(0xFFFF0000);
+                break;
+
             case "Blue":
-            case "Blue Corner":
-            case "Blue Center":
+                mContentView.addView(new ScoringButton(this, "Center Goal"));
+                mContentView.addView(new ScoringButton(this, "Corner Goal"));
                 mContentView.setBackgroundColor(0xFF0000FF);
                 break;
-            default:
-                mContentView.setBackgroundColor(0xFFF57E25);
-        }
+            case "Blue Corner":
+                mContentView.addView(new ScoringButton(this, "Corner Goal"));
+                mContentView.setBackgroundColor(0xFF0000FF);
+                break;
+            case "Blue Center":
+                mContentView.addView(new ScoringButton(this, "Center Goal"));
+                mContentView.setBackgroundColor(0xFF0000FF);
+                break;
 
-        mContentView.addView(new ScoringButton(this, "Center Goal"));
-        mContentView.addView(new ScoringButton(this, "Corner Goal"));
+            case "2 Corners":
+                mContentView.addView(new ScoringButton(this, "Red Corner Goal"));
+                mContentView.addView(new ScoringButton(this, "Blue Corner Goal"));
+                mContentView.setBackgroundColor(0xFFF57E25);
+                break;
+
+            case "2 Centers":
+                mContentView.addView(new ScoringButton(this, "Red Center Goal"));
+                mContentView.addView(new ScoringButton(this, "Blue Center Goal"));
+                mContentView.setBackgroundColor(0xFFF57E25);
+                break;
+
+            case "All Goals":
+                mContentView.addView(new ScoringButton(this, "Red Center Goal"));
+                mContentView.addView(new ScoringButton(this, "Red Corner Goal"));
+                mContentView.addView(new ScoringButton(this, "Blue Center Goal"));
+                mContentView.addView(new ScoringButton(this, "Blue Corner Goal"));
+                mContentView.setBackgroundColor(0xFFF57E25);
+                break;
+        }
     }
 
     class ScoringButton extends TableRow {
@@ -71,6 +109,16 @@ public class ScoringActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     button.setText(String.format(Locale.US, "%s\n%d", goalName, ++score));
+                    view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                }
+            });
+            button.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    if (score > 0) {
+                        button.setText(String.format(Locale.US, "%s\n%d", goalName, --score));
+                    }
+                    return true;
                 }
             });
             this.addView(button);
@@ -79,6 +127,14 @@ public class ScoringActivity extends AppCompatActivity {
         public int getScore() {
             return score;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        for (int i = 0; i < mContentView.getChildCount(); ++i) {
+            Log.d("ScoringActivity", String.valueOf(((ScoringButton) mContentView.getChildAt(i)).getScore()));
+        }
+        super.onBackPressed();
     }
 
     @Override
